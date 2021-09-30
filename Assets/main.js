c291809455d0b27a12960b743ce7de81bfa24173
@bagapillai <pbperumal@gmail.com>
2021-09-29 23:36:54 -0600

    set of codes with moment.js

--- Assets/main.js
+++ Assets/main.js
@@ -1,2 +1,21 @@
-$(document).ready(function () {
-  var hour = moment().hours()}
+$(document).ready(function ()
+{
+var hour = moment().hours();
+
+  function getDate() {
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  };
+
+  function colorData() {
+    $("input").each(function () {
+      var r_Hour = $(this).attr("id");
+      var r_Number = parseInt(r_Hour);
+      if (r_Number === hour) {
+        $(this).addClass("present");
+      } else if (r_umber < hour) {
+        $(this).addClass("past");
+      } else {
+        $(this).addClass("future");
+      };
+    });
+  });
